81808749393981b0686b56d13439e0249da72b0c
B66780 <andrewrodriguez@fullsail.edu>
2014-04-17 21:26:25 -0400

    added files to repo.

---
+++ Rodriguez_Andrew_Assignment3.js
@@ -0,0 +1,27 @@
+//alert("JavaScript works!");
+
+// Andrew Rodriguez
+// Sdi - 1404
+// Assignment 3 Project 3
+// JavaScript - Flowchart
+// " Dragon ", " Slaying", "Armor"
+
+// Preparing to fight a dragon! Deciding which armor to get and what weapon we will use our money on.
+
+// Variables
+
+var myname = "Andrew";
+var light = "Lightweight";
+var heavy = "heavyduty";
+var weapon1 = "Sword";
+var weapon2 = "CrossBow";
+var numberofdragons = 1;
+var fightingadragon = true;
+var money = 150;
+var swordcost = 50;
+var CrossBowcost = 40;
+var Lightweightcost = 75;
+var heavydutycost =50;
+var Prepare = " I will go to the store to purchase our weapons and armor to fight the dragons"; // string
+var Thejourney = " time to fight some dragon\'s"; // escape string
+
